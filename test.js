90886cb67c6247cfd1d1b1eaf0e4f8fe7a865799
lolotrgeek <jon@simplyenable.com>
2022-04-15 08:08:44 -0500

    tests: multitry

--- test.js
+++ test.js
@@ -8,7 +8,8 @@ const { retrier } = require("./main")
  async function retry_test(something, args = []) {
     try {
         let result = await retrier(something, args, 1, 5)
-        console.log("result:", result)
+        console.log(`${something.name} result:`, result)
+        return result
     } catch (error) {
         console.error(error)
     }
@@ -40,9 +41,31 @@ function somethingIsTrue() {
     return true
 }
 
+function somethingIsNull(){
+    return null
+}
+let null_tries = 0
+function somethingToNull() {
+    null_tries++
+    if(null_tries < 3) return false
+    return null
+}
+
+async function multiTry(){
+    let tried = await retry_test(somethingToTry)
+    if (tried === true){
+        await retry_test(somethingToNull)
+        return "PASS"
+    }
+    else return "FAIL"
+}
+
 // retry_test(somethingToTry)
 // retry_test(somethingIsTrue)
 // retry_test(somethingToTry)
 // retry_test(somethingToFail)
+// retry_test(somethingIsNull)
+// retry_test(somethingToNull)
 // retry_test(somethingWithArg, "hi")
-retry_test(somethingWithArgs, ["hi", "world"])
+// retry_test(somethingWithArgs, ["hi", "world"])
+retry_test(multiTry)
